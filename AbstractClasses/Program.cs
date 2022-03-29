d24957c094b1639e330adfcf6e824b5dedefedb1
Demix88 <skrasnauskis@gmail.com>
2022-03-29 15:13:06 +0100

    Add project files.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+          Employee Employee = new Employee
+            {
+                FirstName = "Student",
+                LastName = "Sample"
+            };
+
+            Employee.SayName();
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
